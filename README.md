50df32a0e74ecb65e78b4ca4235bb35d26888663
ErinCleaver-GRCC <erincleaver@email.grcc.edu>
2021-07-25 18:18:31 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-uabx8p
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-uabx8p)
---
+++ index.js
@@ -0,0 +1,22 @@
+let matrix = [
+  [1, 2, '#'],
+  ['#', 16, 32],
+  [64, '#', 256],
+  [512, 1024, '#'],
+  [4096, '#', 16384]
+]
+
+console.log(matrix[0][2])
+
+num = 0;
+for(let i = 0; i < matrix.length; i++) {
+  console.log('testing')
+  for(let j = 0; j < matrix.length; j++) {
+    if(matrix[i][j] == '#') {
+      matrix[i][j]= 2 ** num;
+    }
+    num++;
+  }
+  num-=2;
+  console.log(matrix[i])
+}
